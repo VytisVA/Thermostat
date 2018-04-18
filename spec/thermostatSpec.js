b618aa0fac931eb1536568e0969626099eae5085
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-18 13:31:15 +0100

    node.js, first tests

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Thermostat starts at 20 degrees
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature).toEqual(20);
+  });
+});
