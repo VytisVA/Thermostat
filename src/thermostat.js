6eb60eed32554322a197cc9b17eb498d4b2b08b5
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-04-18 20:26:49 +0100

    thermostat's current energy usage, two tests failing

--- src/thermostat.js
+++ src/thermostat.js
@@ -8,6 +8,7 @@ function Thermostat() {
     this.powerSavingMode = true;
     this.DEFAULT_TEMPERATURE = 20;
     this.temperature = this.DEFAULT_TEMPERATURE;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -57,8 +58,18 @@ Thermostat.prototype.up = function() {
   
   Thermostat.prototype.resetTemperature = function() {
     this.temperature = this.DEFAULT_TEMPERATURE;
-  }  
+  }
+  
+  Thermostat.prototype.energyUsage = function() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
+  }; 
 
-// var thermostat = new Thermostat();
+var thermostat = new Thermostat();
  
-// console.log(thermostat.PSMoff());
+console.log(thermostat.energyUsage());
